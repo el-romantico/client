b6a5fbc3623dfdd15b21e5384482182013e85c2f
Martin Doychev <martin.doychev@abv.bg>
2016-01-30 01:29:07 +0200

    Wait for server dialog

--- Client/app/src/main/java/com/elromantico/client/StartActivity.java
+++ Client/app/src/main/java/com/elromantico/client/StartActivity.java
@@ -1,5 +1,6 @@
 package com.elromantico.client;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ public class StartActivity extends AppCompatActivity {
 
     private LinearLayout startButton;
 
+    private AlertDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,7 @@ public class StartActivity extends AppCompatActivity {
 
             @Override
             public void Handle(int runeIndex) {
+                dialog.hide();
                 Intent intent = new Intent(StartActivity.this, GameActivity.class);
                 startActivity(intent);
             }
@@ -31,6 +35,12 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 RitualsHub.Instance().Connect();
+                dialog = new AlertDialog.Builder(StartActivity.this)
+                        .setTitle("Waiting for server...")
+                        .setMessage("Please wait for the server to start the game...")
+                        .setCancelable(false)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
     }
